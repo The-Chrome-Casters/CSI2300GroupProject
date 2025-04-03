9433ecbaa48a0696a82e316e0fedecff739a8860
ThomasLewis314 <bosspro1729@gmail.com>
2025-04-03 13:20:11 -0400

    Update RandomID.java
    Changed the file from having a main function to having a method, preferably this method would be in our superclass so that we can override it for each subclass

--- RandomID.java
+++ RandomID.java
@@ -1,15 +1,18 @@
 import java.util.Random;
 
 public class RandomID {
-    public static void main(String[] args) {
+
+    public static String create()
+    {
         Random rand = new Random();
         StringBuilder new_ID = new StringBuilder();
-        
-        for (int i = 0; i < 8; i++) {
-            new_ID.append(rand.nextInt(10));
+
+        for (int i = 0; i < 8; ++i)
+        {
+            new_ID.append(rand.nextInt(10));    
         }
         String ID = new_ID.toString();
-        System.out.println(ID);
+        return ID;
     }
 }
 
