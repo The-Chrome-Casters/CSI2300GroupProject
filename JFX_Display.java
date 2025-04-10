a5ada5fa5e8de10f026a26af9bca18b409e934e1
nicholaswriter <nwriter@oakland.edu>
2025-04-10 13:14:43 -0400

    Update JFX_Display.java

--- JFX_Display.java
+++ JFX_Display.java
@@ -1,9 +1,14 @@
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class JavaFX_Display extends Application {
     public static void main(String[] args) {
@@ -77,6 +82,7 @@ public class JavaFX_Display extends Application {
         Button addBook = new Button("Add Book");
         addBook.setLayoutX(150);
         addBook.setLayoutY(300);
+        addBook.setOnAction(e -> addBook(primaryStage));
 
         Button removeBook = new Button("Remove Book");
         removeBook.setLayoutX(350);
@@ -117,6 +123,96 @@ public class JavaFX_Display extends Application {
         Button returnCD = new Button("Return CD");
         returnCD.setLayoutX(750);
         returnCD.setLayoutY(300);
+ 
+        cdPane.getChildren().addAll(backButton, addCD, removeCD, checkoutCD, returnCD);
+        Scene userScene = new Scene(cdPane, 1000, 700);
+        primaryStage.setScene(userScene);
+    }
+
+    private void addBook(Stage primaryStage) {
+        Stage addBookStage = new Stage();
+        addBookStage.setTitle("Add Book Info");
+
+        GridPane addBookGrid = new GridPane();
+        addBookGrid.setVgap(10);
+        addBookGrid.setHgap(10);
+        addBookGrid.setPadding(new javafx.geometry.Insets(10));
+
+        Label titleLabel = new Label ("Title: ");
+        TextField titleField = new TextField();
+
+        Label authorLabel = new Label ("Author: ");
+        TextField authorField = new TextField();
+
+        Label pageLabel = new Label ("Page Count: ");
+        TextField pageField = new TextField();
+
+        Label quantityLabel = new Label ("Quantity: ");
+        TextField quantityField = new TextField();
+
+        Label publishDateLabel = new Label ("Publish Date: ");
+        TextField publishDateField = new TextField();
+
+        Label publisherLabel = new Label ("Publisher: ");
+        TextField publisherField = new TextField();
+
+        addBookGrid.add(titleLabel, 0, 0);
+        addBookGrid.add(titleField, 1, 0);
+
+        addBookGrid.add(authorLabel, 0, 1);
+        addBookGrid.add(authorField, 1, 1);
+
+        addBookGrid.add(pageLabel, 0, 2);
+        addBookGrid.add(pageField, 1, 2);
+
+        addBookGrid.add(quantityLabel, 0, 3);
+        addBookGrid.add(quantityField, 1, 3);
+
+        addBookGrid.add(publishDateLabel, 0, 4);
+        addBookGrid.add(publishDateField, 1, 4);
+
+        addBookGrid.add(publisherLabel, 0, 5);
+        addBookGrid.add(publisherField, 1, 5);
+
+        Button finishButton = new Button ("Finish");
+        addBookGrid.add(finishButton, 1, 6);
+        finishButton.setOnAction (e -> {
+            try {
+                addBookStage.close();
+                String title = titleField.getText();
+                String author = authorField.getText();
+                int pageCount = Integer.parseInt(pageField.getText());
+                int quantity = Integer.parseInt(quantityField.getText());
+                String publishDate = publishDateField.getText();
+                String publisher = publisherField.getText();
+
+                Alert infoAlert = new Alert(Alert.AlertType.INFORMATION);
+                infoAlert.setTitle("Book Information");
+                infoAlert.setHeaderText("New Book Added:");
+                infoAlert.setContentText(
+                "Title: " + title + "\n" +
+                "Author: " + author + "\n" +
+                "Page Count: " + pageCount + "\n" +
+                "Quantity: " + quantity + "\n" +
+                "Publish Date: " + publishDate + "\n" +
+                "Publisher: " + publisher
+            );
+            infoAlert.showAndWait();
+
+            } catch (Exception ex) {
+                Alert errorAlert = new Alert(AlertType.ERROR);
+                errorAlert.setTitle("Invalid Input");
+                errorAlert.setHeaderText("Number Format Error");
+                errorAlert.setContentText("You have entered some information improperly.");
+                errorAlert.showAndWait();
+            } 
+        });
+
+        Scene scene = new Scene(addBookGrid, 300, 250);
+        addBookStage.setScene(scene);
+        addBookStage.show();
+    }
+}
  
         cdPane.getChildren().addAll(backButton, addCD, removeCD, checkoutCD, returnCD);
         Scene userScene = new Scene(cdPane, 1000, 700);
