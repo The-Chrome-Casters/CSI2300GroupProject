408033911b9224f0995c3d95844805e8fa7c9aa8
nicholaswriter <nwriter@oakland.edu>
2025-04-16 17:08:42 -0400

    Update JFX_Display.java

--- JFX_Display.java
+++ JFX_Display.java
@@ -1,3 +1,5 @@
+import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
